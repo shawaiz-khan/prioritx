025fd3e87445d4d60a1b8b0d198479afada38431
Shawaiz Khan <shawaizkhan.biz@gmail.com>
2024-11-10 16:25:55 +0000

    add: Added the pages required

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ErrorPage from './pages/ErrorPage';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Settings from './pages/Settings';
+import Dashboard from './pages/Dashboard';
+import Tasks from './pages/Tasks';
+import AddTask from './pages/AddTask';
+import Completed from './pages/Completed';
+import Pending from './pages/Pending';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='*' element={<ErrorPage />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/sign-up' element={<Signup />} />
+        <Route path='/setting' element={<Settings />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/dashboard/todo' element={<Tasks />} />
+        <Route path='/dashboard/todo/add' element={<AddTask />} />
+        <Route path='/dashboard/completed' element={<Completed />} />
+        <Route path='/dashboard/pending' element={<Pending />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
