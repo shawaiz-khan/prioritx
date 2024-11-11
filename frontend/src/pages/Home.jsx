d47f0006f191b4a81ef006b3c7d014fa588757fc
Shawaiz Khan <shawaizkhan.biz@gmail.com>
2024-11-11 16:53:37 +0000

    add: Route testing component added

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,7 @@
-import RouteTest from "../test/RouteTest";
-
 export default function Home() {
     return (
         <main className="min-h-screen bg-red-200">
             Home
-            <RouteTest />
         </main>
     )
 }
