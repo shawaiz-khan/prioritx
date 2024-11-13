814551ba07d4f00ec9f870aaf434dcfebd52cf7f
Shawaiz Khan <shawaizkhan.biz@gmail.com>
2024-11-13 04:46:21 +0000

    update: Updated the navbar by user login

--- frontend/src/components/UserPfp.jsx
+++ frontend/src/components/UserPfp.jsx
@@ -24,19 +24,19 @@ export default function UserPfp() {
         <div className="relative">
             <button
                 id="user-pfp-btn"
-                className="m-0 relative rounded-full bg-white border border-gray-500 text-purple-700 h-12 w-12 flex justify-center items-center"
+                className="m-0 relative rounded-full bg-white border border-gray-500 text-purple-700 h-10 w-10 flex justify-center items-center"
                 onClick={handleClick}
                 aria-expanded={isMenu}
                 aria-controls="user-pfp-menu"
             >
                 {/* <UserRound size={25} /> */}
-                <img src={userIcon} alt="User" className="w-5" />
+                <img src={userIcon} alt="User" className="w-4" />
             </button>
 
             {isMenu && (
                 <ul
                     id="user-pfp-menu"
-                    className="absolute bg-neutral-100 border border-gray-400 top-10 mt-3 right-7 h-fit px-2 py-3 w-56 rounded-md shadow-md"
+                    className="absolute bg-neutral-100 border border-gray-400 top-10 mt-3 right-5 h-fit px-2 py-3 w-56 rounded-md shadow-md"
                 >
                     <li className="bg-transparent px-3 text-purple-700 font-medium cursor-pointer text-lg">Username</li>
                     <hr className="border-gray-300 mx-3 my-2" />
