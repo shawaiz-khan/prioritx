4b934dea359c5b89278ce31b0949427852019ea0
Shawaiz Khan <shawaizkhan.biz@gmail.com>
2024-11-15 10:43:09 +0000

    update: Feature block updated

--- frontend/src/components/FeaturesBlock.jsx
+++ frontend/src/components/FeaturesBlock.jsx
@@ -2,19 +2,19 @@
 export default function FeaturesBlock({ features }) {
     return (
         <section className="px-5 py-10 bg-gray-50">
-            <h2 className="text-2xl text-center font-semibold text-gray-800 mb-8">Don’t let your day doing nothing</h2>
+            <h2 className="text-2xl text-center font-bold text-gray-950 mb-10">Don’t let your day doing nothing</h2>
             <div className="flex justify-center items-center w-full">
                 <div className="flex justify-between items-center w-1/2">
                     {features.map((feature) => {
                         const { id, data, image } = feature;
                         return (
-                            <div key={id} className="flex flex-col justify-center items-center">
+                            <div key={id} className="flex flex-col justify-center items-center gap-4 text-gray-800 hover:text-gray-800 hover:scale-105 transition-transform duration-300">
                                 <img
                                     src={image}
                                     alt={"Feature image"}
                                     className="w-14 h-14"
                                 />
-                                <p className="text-lg text-gray-800">{data}</p>
+                                <p className="text-lg font-semibold">{data}</p>
                             </div>
                         );
                     })}
