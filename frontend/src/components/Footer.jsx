3280b43f9018d917a2f64408d650838a3d0ae1c9
Shawaiz Khan <shawaizkhan.biz@gmail.com>
2024-11-17 22:37:04 +0000

    update: Updated the footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,8 +1,11 @@
+import { useThemeContext } from '../contexts/ThemeContext'
+
 export default function Footer() {
+    const { isDark } = useThemeContext();
     const year = (new Date().getFullYear().toLocaleString()).split(',');
 
     return (
-        <div className="p-3 bg-light-container text-sm flex justify-center items-center">
+        <div className={`p-3 bg-light-container text-sm flex justify-center items-center border border-t ${isDark ? 'border-gray-700' : ''}`}>
             Copyright © {year} | <strong>&nbsp;Prioritx</strong>
         </div>
     )
