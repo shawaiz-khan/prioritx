4f7f4341295153bcd6ffb99d4903bf30c840bd25
Shawaiz Khan <shawaizkhan.biz@gmail.com>
2024-11-22 09:33:22 +0000

    add: Added the tasks page

--- frontend/src/pages/Tasks.jsx
+++ frontend/src/pages/Tasks.jsx
@@ -9,21 +9,20 @@ export default function Tasks() {
     }
 
     return (
-        <div className="min-h-screen bg-light-background flex">
+        <div className="min-h-screen bg-light-background flex overflow-hidden">
             <div className={`flex flex-col bg-light-container h-screen p-5 ${isExpanded ? "w-2/4" : "w-full"} flex-shrink-0`}>
-                <h1>All Tasks</h1>
-                <div className='flex gap-5 items-center flex-wrap'>
+                <div className={`items-center grid ${isExpanded ? 'grid-cols-2' : 'grid-cols-4'} gap-2`}>
                     {tasks.map((task) => {
-                        const { id, title, description, completed, pending, dueDate, priority } = task;
+                        const { id, title, description, dueDate } = task;
+                        const shortDescription = description.length > 100 ? description.slice(0, 100) + '...' : description;
                         return (
-                            <div key={id} className='bg-red-200 border my-4 p-3'>
-                                <h1>{title}</h1>
-                                <p>{description}</p>
-                                <p>{completed}</p>
-                                <p>{pending}</p>
-                                <p>{dueDate}</p>
-                                <p>{priority}</p>
-                                <button onClick={handlePreviewPane}>Read More</button>
+                            <div key={id} className='bg-light-background border-2 rounded-md p-3 w-full flex flex-col gap-2'>
+                                <h1 className='font-semibold'>{title}</h1>
+                                <p className='font-normal'>{shortDescription}</p>
+                                <div className='flex justify-between items-center'>
+                                    <button onClick={handlePreviewPane} className='text-sm bg-purple-600 hover:bg-purple-700 py-1 px-2 rounded-md text-light-background'>Read More</button>
+                                    <p className='text-gray-400 text-xs'>{dueDate}</p>
+                                </div>
                             </div>
                         )
                     })}
