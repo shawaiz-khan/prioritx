e12503c65f34dfc40a997262622e8636798b978b
Shawaiz Khan <shawaizkhan.biz@gmail.com>
2024-11-24 03:22:24 +0000

    update: Added the form functionality

--- frontend/src/pages/AddTask.jsx
+++ frontend/src/pages/AddTask.jsx
@@ -1,8 +1,35 @@
+import { useState } from 'react';
+import tasks from '../data/SampleTasks';
+
 export default function AddTask() {
+    const newId = () => {
+        const maxId = tasks.reduce((max, task) => {
+            return task.id > max ? task.id : max;
+        }, 0);
+        return maxId + 1;
+    };
+
+    const [newTask, setNewTask] = useState({
+        id: newId(),
+        title: '',
+        description: '',
+        completed: false,
+        dueDate: '',
+        priority: ''
+    });
+
+    const handleOnChange = (e) => {
+        setNewTask({ ...newTask, [e.target.name]: [e.target.value] });
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    }
+
     return (
-        <main className="p-5 flex justify-center items-center h-full bg-light-background">
-            <form className="bg-light-container flex flex-col w-1/2 p-5 rounded-md gap-4">
-                <div>
+        <main className="p-5 flex justify-center items-center min-h-screen bg-light-background">
+            <form className="bg-light-container flex flex-col w-3/4 p-5 rounded-md gap-4">
+                <div className='w-1/4'>
                     <h1 className="text-3xl font-semibold mb-2">Add New Task</h1>
                     <div className="border-t-2 border-purple-600 mb-4"></div>
                 </div>
@@ -15,6 +42,8 @@ export default function AddTask() {
                         className="px-3 py-2 focus:outline-purple-600 rounded-sm border border-gray-300"
                         placeholder="Enter task title"
                         required
+                        onChange={handleOnChange}
+                        value={newTask.title}
                     />
                 </label>
                 <label htmlFor="description" className="flex flex-col gap-2">
@@ -25,6 +54,8 @@ export default function AddTask() {
                         className="px-3 py-2 focus:outline-purple-600 rounded-sm border border-gray-300 h-40 resize-none"
                         placeholder="Enter task description"
                         required
+                        onChange={handleOnChange}
+                        value={newTask.description}
                     ></textarea>
                 </label>
                 <div className="flex justify-between gap-4">
@@ -36,6 +67,8 @@ export default function AddTask() {
                             type="date"
                             className="px-3 py-2 focus:outline-purple-600 rounded-sm border border-gray-300 h-10"
                             required
+                            onChange={handleOnChange}
+                            value={newTask.dueDate}
                         />
                     </label>
                     <label htmlFor="priority" className="flex flex-col gap-2 w-full sm:w-1/2">
@@ -45,6 +78,8 @@ export default function AddTask() {
                             name="priority"
                             className="px-3 py-2 focus:outline-purple-600 rounded-sm border border-gray-300 h-10"
                             required
+                            onChange={handleOnChange}
+                            value={newTask.priority}
                         >
                             <option value="high">High</option>
                             <option value="medium">Medium</option>
@@ -55,6 +90,7 @@ export default function AddTask() {
                 <button
                     type="submit"
                     className="mt-4 px-6 py-2 bg-purple-600 text-white font-semibold rounded-md hover:bg-purple-700"
+                    onSubmit={handleSubmit}
                 >
                     Submit
                 </button>
