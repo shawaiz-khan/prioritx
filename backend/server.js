f72974f0c125c961853f4b2446f5abe6408ff1e7
Shawaiz Khan <shawaizkhan.biz@gmail.com>
2024-11-27 14:55:35 +0000

    update: Updated the server.js and fixed the issues

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect(process.env.MONGODB_URL, {}).then(() => {
+    console.log('Connect to MongoDB');
+}).catch((err) => {
+    console.log('Error: ', err);
+});
+
+app.get('/', (req, res) => {
+    res.send('Prioritx is up and Running');
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
