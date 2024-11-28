3886ea8dcf666dc7b7f2d2198eed71808d0301f0
Shawaiz Khan <shawaizkhan.biz@gmail.com>
2024-11-28 04:37:19 +0000

    fix: Making it ready to deploy

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.get('/', (req, res) => {
     res.send('Prioritx is up and Running');
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
