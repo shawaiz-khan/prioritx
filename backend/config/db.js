2562f02525e6d1280036b3dd98414978e2ba7488
Mupa1 <mupasmail@gmail.com>
2024-12-14 12:24:26 +0100

    ensure mongoURL exists to fix error when connecting

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,19 +1,24 @@
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
 dotenv.config();
 
+const mongoURI = process.env.MONGODB_URL;
+if (!mongoURI) {
+  console.error("MongoDB URI is not defined in environment variables");
+  process.exit(1);
+}
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGODB_URL, {
-            serverSelectionTimeoutMS: 20000,
-        });
-        console.log('Connected to MongoDB');
-    } catch (err) {
-        console.error('Error connecting to MongoDB:', err.message);
-        process.exit(1);
-    }
+  try {
+    await mongoose.connect(mongoURI, {
+      serverSelectionTimeoutMS: 20000,
+    });
+    console.log("Connected to MongoDB");
+  } catch (err) {
+    console.error("Error connecting to MongoDB:", err.message);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
