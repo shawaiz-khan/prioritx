dca1bddbd0a0b40f4058358ef4d29fe9ad6d3126
Shawaiz Khan <shawaizkhan.biz@gmail.com>
2025-01-25 07:16:27 +0000

    update: Updated cors in the server to access the localhost frontend and the deployed frontend request

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,11 @@ connectDB();
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: ["http://localhost:5173", "https://prioritx.vercel.app", "https://prioritx-production.up.railway.app"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+}));
 
 app.use('/api/tasks', taskRoutes);
 app.use('/api/users', userRoutes);
