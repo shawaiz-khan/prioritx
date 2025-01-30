85bf31cec3255f2b95d7ae901553c06da3790261
Shawaiz Khan <shawaizkhan.biz@gmail.com>
2025-01-30 06:15:51 +0000

    update: Updated the navbar and footer to respond on theme toggle

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
     const year = (new Date().getFullYear().toLocaleString()).split(',');
 
     return (
-        <div className={`p-3 bg-light-container text-sm flex justify-center items-center border border-t ${isDark ? 'border-gray-700' : ''}`}>
+        <div className={`p-3 text-sm flex justify-center items-center border border-t ${isDark ? 'border-gray-700 bg-dark-container text-light-background' : ''}`}>
             Copyright © {year} | <strong>&nbsp;Prioritx</strong>
         </div>
     )
